91284ffd2d5d6bd12454062f376d0f06452d0a32
Abin Karukappallil <followgamer102@gmail.com>
2024-10-14 23:05:37 +0530

    [FEAT] Added Dashboard

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './HomeScreen'; 
 import login_screen from './login_screen/login_screen';
 import SignUpScreen from './login_screen/signup_screen';
+import Dashboard from './dashboard/dash';
+
 
 const Stack = createNativeStackNavigator();
 
@@ -14,6 +16,7 @@ const App = () => {
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Login" component={login_screen} /> 
         <Stack.Screen name="Signup"component={SignUpScreen} />
+        <Stack.Screen name="dashboard" component={Dashboard} />
       </Stack.Navigator>
     </NavigationContainer>
   );
