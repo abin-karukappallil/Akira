1d4542582aa47f7c27db8296757060792bace51b
Abin Thomas <followgamer102@gmail.com>
2025-02-15 18:04:18 +0530

    fix: some minor fix and added mock backend

--- back/api.py
+++ back/api.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 from typing import Optional
 import sqlite3
 
-SECRET_KEY = "your_secret_key"
+SECRET_KEY = "sample"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
